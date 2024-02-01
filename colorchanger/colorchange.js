6c0bf686761cd3a35c086a6f0bf5be40ba45fbcd
Pranali209 <69621580+Pranali209@users.noreply.github.com>
2024-02-01 17:11:42 +0530

    Delete colorchanger directory

--- colorchanger/colorchange.js
+++
@@ -1,7 +0,0 @@
-const change = (event) =>
-{
-  clickedDiv = event.target; // we are passing the event on every click and  den selecting the  element by doing event.target
-  const computatedStyle = window.getComputedStyle(clickedDiv);// this line will give the style of the selected div
-  document.body.style.backgroundColor  = computatedStyle.backgroundColor;
-
-}
